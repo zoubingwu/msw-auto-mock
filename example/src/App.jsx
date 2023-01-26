4caa412b4f747387f63aab29a469530bcc141aa5
zoubingwu <zoubingwu@gmail.com>
2023-01-27 00:10:22 +0800

    fix(exmaple): add method select

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,30 +1,56 @@
 import { useState } from 'react';
 
+const methods = [
+  'GET',
+  'POST',
+  'PUT',
+  'PATCH',
+  'DELETE',
+  'HEAD',
+  'OPTIONS',
+  'TRACE',
+];
+
 function App() {
+  const [method, setMethod] = useState(methods[0]);
   const [endpoint, setEndpoint] = useState('/admin/tokens');
   const [res, setRes] = useState(null);
+  const [status, setStatus] = useState(null);
 
   return (
     <div className="App">
       <h2>GitHub API mock playground</h2>
+      <label>Method</label>
+      <select value={method} onChange={e => setMethod(e.target.value)}>
+        {methods.map(i => (
+          <option key={i} value={i}>
+            {i}
+          </option>
+        ))}
+      </select>
+      <br />
       <label>Endpoint: </label>
       <input
         type="text"
         value={endpoint}
         onChange={e => setEndpoint(e.target.value)}
       />
+      <br />
       <button
         onClick={async () => {
-          const res = await fetch(endpoint);
+          const res = await fetch(endpoint, { method });
           const data = await res.json();
           setRes(data);
+          setStatus(res.status);
         }}
       >
         fetch
       </button>
 
+      {status && <div>Status: {status}</div>}
+
       <pre>
-        <code>{res ? JSON.stringify(res, null, 4) : null}</code>
+        <code>{res ? JSON.stringify(res, null, 4) : 'null'}</code>
       </pre>
     </div>
   );
