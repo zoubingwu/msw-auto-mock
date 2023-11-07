2a5b9dcd8678d6b227ad91b7232a89631fcaf6b3
Guillaume <4112243+LEI@users.noreply.github.com>
2023-11-07 19:50:07 +0100

    test: example snapshot fix async and increase timeout

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,11 +1,28 @@
+import { promisify } from 'util';
 import { exec } from 'child_process';
 import { describe, expect, test } from 'vitest';
+
+const escapedCwd = process.cwd().replace(/\\/g, '\\\\');
+const execAsync = promisify(exec);
+const timeout = 20000;
+
+expect.addSnapshotSerializer({
+  serialize(val, config, indentation, depth, refs, printer) {
+    return printer(val, { ...config, plugins: [] }, indentation, depth, refs)
+      .split(escapedCwd)
+      .join('.')
+      .replace(/\\\\/g, '/');
+  },
+  test(val) {
+    return val && Object.prototype.hasOwnProperty.call(val, 'startWorker');
+  },
+});
 
 async function generateExample(name, args = '') {
   const fixture = `./test/fixture/${name}.yaml`;
   const output = `./example/test-${name}${args.replace(/\s+/g, '-')}.js`;
   const command = `node ./dist/cli.js ${fixture} --output ${output} ${args}`;
-  await exec(command, { cwd: '.' });
+  await execAsync(command, { cwd: '.' });
   return output;
 }
 
@@ -13,8 +30,12 @@ describe('example', () => {
   test.each([
     { name: 'githubapi', args: '--node' },
     { name: 'githubapi', args: '--react-native' },
-  ])('$name $args should be valid', async ({ name, args }) => {
-    const example = await generateExample(name, args);
-    await expect(import(example)).resolves.toMatchObject({});
-  });
+  ])(
+    '$name $args should be valid',
+    async ({ name, args }) => {
+      const example = await generateExample(name, args);
+      await expect(import(example)).resolves.toMatchSnapshot();
+    },
+    timeout
+  );
 });
